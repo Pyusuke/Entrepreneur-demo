c95bb34887248de827d6d43eab5bf6013f9c29e0
Pyusuke <wancorocookie@gmail.com>
2026-07-08 18:50:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   新しいSWがactivateされた時点で旧キャッシュは削除され、
  *   次回ロードから新しいファイルが配信される。
  */
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const CACHE_NAME = "tankyu-card-game-" + CACHE_VERSION;
 const ASSETS = ["./", "./index.html", "./html2canvas.min.js"];
 
